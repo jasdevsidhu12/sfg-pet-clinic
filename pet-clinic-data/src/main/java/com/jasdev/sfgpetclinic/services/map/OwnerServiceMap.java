47097bb0e53eefcb236607b35cc298cf5e5bd9d5
jasdevsidhu12 <jasdev.project.12@gmail.com>
2020-12-25 12:00:32 +0000

    Implement map based services. Closes #4.

--- pet-clinic-data/src/main/java/com/jasdev/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/jasdev/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -15,7 +15,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
        super.deleteById(id);
-    }
+    } 
 
     @Override
     public Owner save(Owner owner) {
