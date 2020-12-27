44d2a194a873f049e49ca01fed647c83488e49f8
jasdevsidhu12 <jasdev.project.12@gmail.com>
2020-12-27 13:12:56 +0000

    Implement Spring Config for Services. Closes #22.

--- pet-clinic-web/src/main/java/com/jasdev/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jasdev/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,21 +4,21 @@ import com.jasdev.sfgpetclinic.model.Owner;
 import com.jasdev.sfgpetclinic.model.Vet;
 import com.jasdev.sfgpetclinic.services.OwnerService;
 import com.jasdev.sfgpetclinic.services.VetService;
-import com.jasdev.sfgpetclinic.services.map.OwnerServiceMap;
-import com.jasdev.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerService ownerService;
-    private final VetService vetService;
+    private OwnerService ownerService;
+    private VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
+
+
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
