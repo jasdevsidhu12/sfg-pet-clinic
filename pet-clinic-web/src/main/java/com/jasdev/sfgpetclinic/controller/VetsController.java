ecc06a82fa8a74128dac3287f2bee597345e5942
jasdevsidhu12 <jasdev.project.12@gmail.com>
2020-12-27 13:31:19 +0000

    List all Vets on Vet Index page. Closes #15.

--- pet-clinic-web/src/main/java/com/jasdev/sfgpetclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/jasdev/sfgpetclinic/controller/VetsController.java
@@ -1,12 +1,18 @@
 package com.jasdev.sfgpetclinic.controller;
 
+import com.jasdev.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+    @Autowired
+    VetService vetService;
     @RequestMapping({"/vets", "/vets/index"})
-    public String listView() {
+    public String listView(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
